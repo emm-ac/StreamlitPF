4e17b0d78a36c4fc38bedbc816eade79340c8811
Emmanuel <emmanuel.ac6@gmail.com>
2023-01-31 18:37:17 -0300

    Add files via upload

--- ProductoOlist.py
+++ ProductoOlist.py
@@ -1,15 +1,27 @@
 import streamlit as st
 import base64
 import os
+from flask import Flask, Response
 
 
 
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    csp = "default-src 'self'; img-src 'self' https://example.com; frame-src 'self' https://example.com;"
+    return Response(st.render(), headers={"Content-Security-Policy": csp})
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+#Configuración de la página
 st.set_page_config(page_title='TA Tools - Edades', 
                    page_icon='📊', 
                    layout="wide", 
                    initial_sidebar_state="expanded", 
                    menu_items=None)
-
 
 
 
