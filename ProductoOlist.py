bd0552ca94d8427a39decd2a2649aae419f12b7c
Emmanuel <emmanuel.ac6@gmail.com>
2023-01-31 22:33:11 -0300

    Add files via upload

--- ProductoOlist.py
+++ ProductoOlist.py
@@ -36,30 +36,19 @@ add_bg_from_local(path_img)
 
 
 
+path_web = 'https://docs.google.com/document/d/1T5kXNatlGFI60dV4AihWUWGGr4ybV2cU/edit?usp=sharing&ouid=110999774945235708227&rtpof=true&sd=true'
+st.markdown(f'<iframe src="{path_web}" width="950" height="700"></iframe>', unsafe_allow_html=True)
+
+
+
 #Código para visualizar PDF local    
-path = os.path.join(os.path.dirname(
-    os.path.abspath(__file__)), "./assets/Analisis.pdf")
-    
-def show_pdf(file_path):
-    with open(file_path,"rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="1000" height="800" class_="iframe-center" type="application/pdf"></iframe>'
-    st.markdown(pdf_display, unsafe_allow_html=True)
-
-show_pdf(path)
-
-
-
-#Código para visualizar PDF de la nube    
-path_web = 'https://github.com/emm-ac/StreamlitPF/blob/8ff593b424c0f44a084ea616f1b644b91e624b35/assets/Analisis.pdf'
-st.markdown(f'<iframe src="{path_web}" width="700" height="700"></iframe>', unsafe_allow_html=True)
-
-path_web2 = 'https://drive.google.com/file/d/1ZTXhWUK8tRgwE79U8jXtSzphUJDCmHdP/view?usp=share_link'
-st.markdown(f'<iframe src="{path_web2}" width="700" height="700"></iframe>', unsafe_allow_html=True)
-
-path_web = 'https://docs.google.com/document/d/1T5kXNatlGFI60dV4AihWUWGGr4ybV2cU/edit?usp=share_link&ouid=110999774945235708227&rtpof=true&sd=true'
-st.markdown(f'<iframe src="{path_web}" width="1000" height="700"></iframe>', unsafe_allow_html=True)
-
-
-
-#https://github.com/emm-ac/StreamlitPF/blob/8ff593b424c0f44a084ea616f1b644b91e624b35/assets/Analisis.pdf
+#path = os.path.join(os.path.dirname(
+#    os.path.abspath(__file__)), "./assets/Análisis - Proyecto Olist.pdf")
+#    
+#def show_pdf(file_path):
+#    with open(file_path,"rb") as f:
+#        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+#    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="1000" height="800" class_="iframe-center" type="application/pdf"></iframe>'
+#    st.markdown(pdf_display, unsafe_allow_html=True)
+#
+#show_pdf(path)
